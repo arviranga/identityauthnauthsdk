9ec35a9312f24831fec75c28335f4639da1b2505
Aravindan Ranganathan <aranganathan@paypal.com>
2015-03-14 18:14:03 -0700

    Java-style

--- IdentityAuthnAuthServ/src/main/java/com/arathika/identityauthnauth/api/rest/TokenResponse.java
+++ IdentityAuthnAuthServ/src/main/java/com/arathika/identityauthnauth/api/rest/TokenResponse.java
@@ -7,118 +7,118 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @JsonInclude(Include.NON_NULL)
 public class TokenResponse {
-    
-	@XmlElement(name = "access_token")
-	private String accessToken;
 
-	@XmlElement(name = "token_type")
-	private String tokenType;
+    @XmlElement(name = "access_token")
+    private String accessToken;
 
-	private String scope;
+    @XmlElement(name = "token_type")
+    private String tokenType;
 
-	@XmlElement(name = "expires_in")
-	private Long expiresIn;
+    private String scope;
 
-	@XmlElement(name = "refresh_token")
-	private String refreshToken;
+    @XmlElement(name = "expires_in")
+    private Long expiresIn;
 
-	@XmlElement(name = "id_token")
-	private String idToken;
+    @XmlElement(name = "refresh_token")
+    private String refreshToken;
 
-	@XmlElement(name = "code")
-	private String code;
+    @XmlElement(name = "id_token")
+    private String idToken;
 
-	@XmlElement(name = "nonce")
-	private String nonce;
-    
-	@XmlElement(name = "error")
-	private String errorString;
+    @XmlElement(name = "code")
+    private String code;
 
-	@XmlElement(name = "error_description")
-	private String errorDescription;
-    
+    @XmlElement(name = "nonce")
+    private String nonce;
+
+    @XmlElement(name = "error")
+    private String errorString;
+
+    @XmlElement(name = "error_description")
+    private String errorDescription;
+
     @XmlElement(name = "error_uri")
     private String errorUri;
 
-	public String getAccessToken() {
-		return accessToken;
-	}
+    public String getAccessToken() {
+        return accessToken;
+    }
 
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
-	}
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 
-	public String getTokenType() {
-		return tokenType;
-	}
+    public String getTokenType() {
+        return tokenType;
+    }
 
-	public void setTokenType(String tokenType) {
-		this.tokenType = tokenType;
-	}
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
 
-	public String getScope() {
-		return scope;
-	}
+    public String getScope() {
+        return scope;
+    }
 
-	public void setScope(String scope) {
-		this.scope = scope;
-	}
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
 
-	public Long getExpiresIn() {
-		return expiresIn;
-	}
+    public Long getExpiresIn() {
+        return expiresIn;
+    }
 
-	public void setExpiresIn(Long expiresIn) {
-		this.expiresIn = expiresIn;
-	}
+    public void setExpiresIn(Long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
 
-	public String getRefreshToken() {
-		return refreshToken;
-	}
+    public String getRefreshToken() {
+        return refreshToken;
+    }
 
-	public void setRefreshToken(String refreshToken) {
-		this.refreshToken = refreshToken;
-	}
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 
-	public String getErrorString() {
-		return errorString;
-	}
+    public String getErrorString() {
+        return errorString;
+    }
 
-	public void setErrorString(String errorString) {
-		this.errorString = errorString;
-	}
+    public void setErrorString(String errorString) {
+        this.errorString = errorString;
+    }
 
-	public String getErrorDescription() {
-		return errorDescription;
-	}
+    public String getErrorDescription() {
+        return errorDescription;
+    }
 
-	public void setErrorDescription(String errorDescription) {
-		this.errorDescription = errorDescription;
-	}
+    public void setErrorDescription(String errorDescription) {
+        this.errorDescription = errorDescription;
+    }
 
-	public String getIdToken() {
-		return idToken;
-	}
+    public String getIdToken() {
+        return idToken;
+    }
 
-	public void setIdToken(String idToken) {
-		this.idToken = idToken;
-	}
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
 
-	public String getCode() {
-		return code;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	public String getNonce() {
-		return nonce;
-	}
+    public String getNonce() {
+        return nonce;
+    }
 
-	public void setNonce(String nonce) {
-		this.nonce = nonce;
-	}
+    public void setNonce(String nonce) {
+        this.nonce = nonce;
+    }
 
     /**
      * @return the errorUri
